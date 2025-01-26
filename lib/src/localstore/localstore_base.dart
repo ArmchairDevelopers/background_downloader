5348c62d459084aa81bc6fc061b93b720545a435
Liam <47498574+7reax@users.noreply.github.com>
2025-01-26 12:40:30 +0100

    Update localstore_base.dart

--- lib/src/localstore/localstore_base.dart
+++ lib/src/localstore/localstore_base.dart
@@ -8,7 +8,7 @@ part of 'localstore.dart';
 /// final db = Localstore.instance;
 /// ```
 final class Localstore implements LocalstoreImpl {
-  final _databaseDirectory = getApplicationSupportDirectory();
+  var _databaseDirectory = getApplicationSupportDirectory();
   final _delegate = DocumentRef._('');
   static final Localstore _localstore = Localstore._();
 
